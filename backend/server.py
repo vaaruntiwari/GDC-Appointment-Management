d40a83d406020f741140bcb1d57d402d3cbd6e63
vaaruntiwari <airlearn2048@gmail.com>
2026-08-22 23:33:16 +0530

    Fixing cors-2

--- backend/server.py
+++ backend/server.py
@@ -525,7 +525,7 @@ async def websocket(ws: WebSocket):
 app.include_router(api)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gdc-appointment-management.vercel.app/"],
+    allow_origins=["https://gdc-appointment-management.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
